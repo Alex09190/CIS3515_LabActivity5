54afc9b61706a2c7b6e1e594a947c28e31f013bb
Alex09190 <tuj09190@temple.edu>
2023-10-16 15:19:05 -0400

    Added if/else to catch empty list crash

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -37,9 +37,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<View>(R.id.deleteButton).setOnClickListener {
-            (names as MutableList).removeAt(spinner.selectedItemPosition)
-            (spinner.adapter as BaseAdapter).notifyDataSetChanged()
-        }
 
+            if (names.size > 1) {
+                (names as MutableList).removeAt(spinner.selectedItemPosition)
+                (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+
+            } else {
+                (names as MutableList).add("Empty List")
+            }
+        }
     }
 }
